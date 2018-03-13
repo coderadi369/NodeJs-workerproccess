09583b88b64e28fb4ea09a352c43e1fd14d740dd
coderadi369 <adithya.iiitm@gmail.com>
2018-03-13 22:11:14 +0530

    added a basic db connection

--- github_code.js
+++ github_code.js
@@ -16,22 +16,6 @@ const options={
 let conn=mysql_config.getconnection();
 
 
-
-let search_repo=(req,res)=>{
-    
-   options.uri+=req.query.lang 
-
-   rp(options).then(function(data){
-		res.json({'search-match':data.items})
-	}).catch(function(error){
-    	res.send(error)
-	})
-}
-
-let get_contributors=(req,res)=>{
-	res.send("Building functionality of getting contributors")
-}
-
 module.exports={
 	search_repo:search_repo,
 	get_contributors:get_contributors
